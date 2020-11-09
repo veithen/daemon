ae14eff5ecf733b8ffcac7590e74d3928059e533
Andreas Veithen <andreas.veithen@gmail.com>
2020-11-09 21:56:05 +0000

    Use JUnit for the jetty-request-log integration test

---
+++ daemon-maven-plugin/src/it/jetty-request-log/src/test/java/jetty/JettyITCase.java
@@ -0,0 +1,45 @@
+/*-
+ * #%L
+ * Daemon Tools
+ * %%
+ * Copyright (C) 2012 - 2020 Andreas Veithen
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package jetty;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.junit.Test;
+
+public class JettyITCase {
+    @Test
+    public void test() throws Exception {
+        HttpURLConnection c = (HttpURLConnection)new URL(
+                String.format("http://localhost:%s/somefile.txt", System.getProperty("jetty.httpPort"))).openConnection();
+        assertThat(c.getResponseCode()).isEqualTo(200);
+        c.disconnect();
+    
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("target/request.log"), "utf-8"))) {
+            assertThat(in.readLine()).contains("GET /somefile.txt");
+            assertThat(in.readLine()).isNull();
+        }
+    }
+}
