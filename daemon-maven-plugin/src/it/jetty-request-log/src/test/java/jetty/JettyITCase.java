566b95721965cc4461fc02f14b9f257dfd41c3f5
Andreas Veithen <andreas.veithen@gmail.com>
2020-12-31 10:56:52 +0000

    Update parent POM

--- daemon-maven-plugin/src/it/jetty-request-log/src/test/java/jetty/JettyITCase.java
+++ daemon-maven-plugin/src/it/jetty-request-log/src/test/java/jetty/JettyITCase.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -53,16 +53,25 @@ public class JettyITCase {
 
     @Test
     public void test() throws Exception {
-        HttpURLConnection c = (HttpURLConnection)new URL(
-                String.format("http://localhost:%s/somefile.txt", System.getProperty("jetty.httpPort"))).openConnection();
+        HttpURLConnection c =
+                (HttpURLConnection)
+                        new URL(
+                                        String.format(
+                                                "http://localhost:%s/somefile.txt",
+                                                System.getProperty("jetty.httpPort")))
+                                .openConnection();
         assertThat(c.getResponseCode()).isEqualTo(200);
         c.disconnect();
-    
-        withRetry(() -> {
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("target/request.log"), "utf-8"))) {
-                assertThat(in.readLine()).contains("GET /somefile.txt");
-                assertThat(in.readLine()).isNull();
-            }
-        });
+
+        withRetry(
+                () -> {
+                    try (BufferedReader in =
+                            new BufferedReader(
+                                    new InputStreamReader(
+                                            new FileInputStream("target/request.log"), "utf-8"))) {
+                        assertThat(in.readLine()).contains("GET /somefile.txt");
+                        assertThat(in.readLine()).isNull();
+                    }
+                });
     }
 }
--- daemon-maven-plugin/src/it/jetty/src/test/java/jetty/JettyITCase.java
+++ daemon-maven-plugin/src/it/jetty/src/test/java/jetty/JettyITCase.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,9 +30,13 @@ import org.junit.Test;
 public class JettyITCase {
     @Test
     public void testHelloWorld() throws Exception {
-        URL url = new URL(String.format("http://localhost:%s/HelloWorld",
-                                        System.getProperty("jetty.httpPort")));
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "utf-8"))) {
+        URL url =
+                new URL(
+                        String.format(
+                                "http://localhost:%s/HelloWorld",
+                                System.getProperty("jetty.httpPort")));
+        try (BufferedReader in =
+                new BufferedReader(new InputStreamReader(url.openStream(), "utf-8"))) {
             assertEquals("Hello world!", in.readLine());
         }
     }
